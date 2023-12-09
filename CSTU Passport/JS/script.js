5a5baa07d90d182ad2ec23fa0f8de98625e95d4f
TeamMie4K <Teamthai101@gmail.com>
2023-12-10 00:19:04 +0700

    test

--- CSTU Passport/JS/script.js
+++ CSTU Passport/JS/script.js
@@ -40,222 +40,267 @@ const port = 8000;
 //   .catch(error => console.log(error));
 
   // Function to fetch records from the backend
-async function fetchRecords() {
-  try {
-    const response = await fetch(`http://${window.location.hostname}:${port}/record`);
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      return data;
-    } else {
-      console.error("Failed to fetch records.");
-      return [];
-    }
-  } catch (error) {
-    console.error("An error occurred while fetching records:", error);
-    return [];
-  }
-}
-
-function OnloadRecordDisplay(RecordData){
-  RecordData.forEach(record => {
-    const markup = `<div class="col">
-                      <span>${record.first_name}</span>
-                      <span>${record.last_name}</span>
-                      <h4>${record.student_id}</h4>
-                      <h4>${record.email}</h4>
-                      <h4>${record.title}</h4>
-                      <h4>${record.type_of_work_id}</h4>
-                      <h4>${record.academic_year}</h4>
-                      <h4>${record.semester}</h4>
-                      <h4>${record.start_date}</h4>
-                      <h4>${record.end_date}</h4>
-                      <h4>${record.location}</h4>
-                      <h4>${record.description}</h4>
-                    </div>
-                    `;
-
-    document.querySelector('.row').insertAdjacentHTML('beforeend', markup);
-  });
-}
-
-  // Event listener when the page content has finished loading
-  document.addEventListener("DOMContentLoaded", async () => {
-    const RecordData = await fetchRecords();
-    OnloadRecordDisplay(RecordData);
-  });
-  
-// // Function to validate Firstname and Lastname
-// function validateName() {
-//   const fullnameInput = document.getElementById("fullname");
-//   const names = fullnameInput.value.trim().split(" ");
-//   const errorElement = document.getElementById("fullnameError");
-
-//   if (names.length !== 2) {
-//     errorElement.textContent = "Please enter both your Firstname and Lastname.";
-//     return false;
-//   } else {
-//     errorElement.textContent = ""; // Clear the error message when valid
-//   }
-//   return true;
-// }
-
-// // Function to validate Student ID
-// function validateStudentID() {
-//   const studentIDInput = document.getElementById("studentID");
-//   const studentIDPattern = /^\d{10}$/;
-//   const errorElement = document.getElementById("studentIDError");
-
-//   if (!studentIDPattern.test(studentIDInput.value)) {
-//     errorElement.textContent = "Please enter a 10-digit Student ID.";
-//     return false;
-//   } else {
-//     errorElement.textContent = ""; // Clear the error message when valid
-//   }
-//   return true;
-// }
-
-// // Function to validate University Email
-// function validateEmail() {
-//   const emailInput = document.getElementById("email");
-//   const emailPattern = /^.+@dome\.tu\.ac\.th$/;
-//   const errorElement = document.getElementById("emailError");
-
-//   if (!emailPattern.test(emailInput.value)) {
-//     errorElement.textContent =
-//       "Please provide a valid university email in the format 'xxx.yyy@dome.tu.ac.th'.";
-//     return false;
-//   } else {
-//     errorElement.textContent = ""; // Clear the error message when valid
-//   }
-//   return true;
-// }
-
-// // Function to validate form inputs on user input
-// function validateFormOnInput() {
-//   validateName();
-//   validateStudentID();
-//   validateEmail();
-// }
-
-// // Function to fetch activity types from the backend
-// async function fetchActivityTypes() {
+// async function fetchRecords() {
 //   try {
-//     const response = await fetch(`http://${window.location.hostname}:${port}/getActivityType`);
+//     const response = await fetch(`http://${window.location.hostname}:${port}/record`);
 //     if (response.ok) {
 //       const data = await response.json();
+//       console.log(data);
 //       return data;
 //     } else {
-//       console.error("Failed to fetch activity types.");
+//       console.error("Failed to fetch records.");
 //       return [];
 //     }
 //   } catch (error) {
-//     console.error("An error occurred while fetching activity types:", error);
+//     console.error("An error occurred while fetching records:", error);
 //     return [];
 //   }
 // }
 
-// // Function to populate activity types in the select element
-// function populateActivityTypes(activityTypes) {
-//   const activityTypeSelect = document.getElementById("activityType");
+// function OnloadRecordDisplay(RecordData){
+//   RecordData.forEach(record => {
+//     const markup = `<div class="col">
+//                       <span>${record.first_name}</span>
+//                       <span>${record.last_name}</span>
+//                       <h4>${record.student_id}</h4>
+//                       <h4>${record.email}</h4>
+//                       <h4>${record.title}</h4>
+//                       <h4>${record.type_of_work_id}</h4>
+//                       <h4>${record.academic_year}</h4>
+//                       <h4>${record.semester}</h4>
+//                       <h4>${record.start_date}</h4>
+//                       <h4>${record.end_date}</h4>
+//                       <h4>${record.location}</h4>
+//                       <h4>${record.description}</h4>
+//                     </div>
+//                     `;
 
-//   for (const type of activityTypes) {
-//     const option = document.createElement("option");
-//     option.value = type.id;
-//     option.textContent = type.value;
-//     activityTypeSelect.appendChild(option);
+//     document.querySelector('.row').insertAdjacentHTML('beforeend', markup);
+//   });
+// }
+
+//   // Event listener when the page content has finished loading
+//   document.addEventListener("DOMContentLoaded", async () => {
+//     const RecordData = await fetchRecords();
+//     OnloadRecordDisplay(RecordData);
+//   });
+  
+// Function to validate Firstname and Lastname
+function validateName() {
+  const fullnameInput = document.getElementById("fullname");
+  const names = fullnameInput.value.trim().split(" ");
+  const errorElement = document.getElementById("fullnameError");
+
+  if (names.length !== 2) {
+    errorElement.textContent = "Please enter both your Firstname and Lastname.";
+    return false;
+  } else {
+    errorElement.textContent = ""; // Clear the error message when valid
+  }
+  return true;
+}
+
+// Function to validate Student ID
+function validateStudentID() {
+  const studentIDInput = document.getElementById("studentID");
+  const studentIDPattern = /^\d{10}$/;
+  const errorElement = document.getElementById("studentIDError");
+
+  if (!studentIDPattern.test(studentIDInput.value)) {
+    errorElement.textContent = "Please enter a 10-digit Student ID.";
+    return false;
+  } else {
+    errorElement.textContent = ""; // Clear the error message when valid
+  }
+  return true;
+}
+
+// Function to validate University Email
+function validateEmail() {
+  const emailInput = document.getElementById("email");
+  const emailPattern = /^.+@dome\.tu\.ac\.th$/;
+  const errorElement = document.getElementById("emailError");
+
+  if (!emailPattern.test(emailInput.value)) {
+    errorElement.textContent =
+      "Please provide a valid university email in the format 'xxx.yyy@dome.tu.ac.th'.";
+    return false;
+  } else {
+    errorElement.textContent = ""; // Clear the error message when valid
+  }
+  return true;
+}
+
+// Function to validate form inputs on user input
+function validateFormOnInput() {
+  validateName();
+  validateStudentID();
+  validateEmail();
+}
+
+// Function to fetch activity types from the backend
+async function fetchActivityTypes() {
+  try {
+    const response = await fetch(`http://${window.location.hostname}:${port}/getActivityType`);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      console.error("Failed to fetch activity types.");
+      return [];
+    }
+  } catch (error) {
+    console.error("An error occurred while fetching activity types:", error);
+    return [];
+  }
+}
+
+// Function to populate activity types in the select element
+function populateActivityTypes(activityTypes) {
+  const activityTypeSelect = document.getElementById("activityType");
+
+  for (const type of activityTypes) {
+    const option = document.createElement("option");
+    option.value = type.id;
+    option.textContent = type.value;
+    activityTypeSelect.appendChild(option);
+  }
+}
+
+// Event listener when the page content has finished loading
+document.addEventListener("DOMContentLoaded", async () => {
+  const activityTypes = await fetchActivityTypes();
+  populateActivityTypes(activityTypes);
+});
+
+// Function to submit the form
+// Function to submit the form
+async function submitForm(event) {
+  event.preventDefault();
+
+  // Validate form inputs before submission
+  if (!validateName() || !validateStudentID() || !validateEmail()) {
+    return;
+  }
+
+  const startDateInput = document.getElementById("startDate").value;
+  const endDateInput = document.getElementById("endDate").value;
+  const startDate = new Date(startDateInput);
+  const endDate = new Date(endDateInput);
+
+  if (endDate <= startDate) {
+    alert("End datetime should be after the start datetime.");
+    return;
+  }
+
+// // Function to fetch records from the backend
+// async function fetchRecords() {
+//   try {
+//     const response = await fetch(`http://${window.location.hostname}:${port}/record`);
+//     if (response.ok) {
+//       const data = await response.json();
+//       return data;
+//     } else {
+//       console.error("Failed to fetch records.");
+//       return [];
+//     }
+//   } catch (error) {
+//     console.error("An error occurred while fetching records:", error);
+//     return [];
 //   }
 // }
 
-// // Event listener when the page content has finished loading
-// document.addEventListener("DOMContentLoaded", async () => {
-//   const activityTypes = await fetchActivityTypes();
-//   populateActivityTypes(activityTypes);
-// });
+// function OnloadRecordDisplay(RecordData){
+//   RecordData.forEach(record => {
+//     const markup = `<div class="col">
+//                       <span>${record.first_name}</span>
+//                       <span>${record.last_name}</span>
+//                       <h4>${record.student_id}</h4>
+//                       <h4>${record.email}</h4>
+//                       <h4>${record.title}</h4>
+//                       <h4>${record.type_of_work_id}</h4>
+//                       <h4>${record.academic_year}</h4>
+//                       <h4>${record.semester}</h4>
+//                       <h4>${record.start_date}</h4>
+//                       <h4>${record.end_date}</h4>
+//                       <h4>${record.location}</h4>
+//                       <h4>${record.description}</h4>
+//                     </div>
+//                     `;
 
-// // Function to submit the form
-// // Function to submit the form
-// async function submitForm(event) {
-//   event.preventDefault();
+//     document.querySelector('.row').insertAdjacentHTML('beforeend', markup);
+//   });
+// }
 
-//   // Validate form inputs before submission
-//   if (!validateName() || !validateStudentID() || !validateEmail()) {
-//     return;
-//   }
+//   // Event listener when the page content has finished loading
+//   document.addEventListener("DOMContentLoaded", async () => {
+//     const RecordData = await fetchRecords();
+//     OnloadRecordDisplay(RecordData);
+//   });
 
-//   const startDateInput = document.getElementById("startDate").value;
-//   const endDateInput = document.getElementById("endDate").value;
-//   const startDate = new Date(startDateInput);
-//   const endDate = new Date(endDateInput);
+  // Create the data object to send to the backend
+  const formData = new FormData(event.target);
+  const data = {
+    first_name: formData.get("fullname").split(" ")[0],
+    last_name: formData.get("fullname").split(" ")[1],
+    student_id: parseInt(formData.get("studentID")),
+    email: formData.get("email"),
+    title: formData.get("workTitle"),
+    type_of_work_id: parseInt(formData.get("activityType")),
+    academic_year: parseInt(formData.get("academicYear")) - 543,
+    semester: parseInt(formData.get("semester")),
+    start_date: formData.get("startDate"),
+    end_date: formData.get("endDate"),
+    location: formData.get("location"),
+    description: formData.get("description")
+  };
 
-//   if (endDate <= startDate) {
-//     alert("End datetime should be after the start datetime.");
-//     return;
-//   }
+  console.log(data);
 
-// // // Function to fetch records from the backend
-// // async function fetchRecords() {
-// //   try {
-// //     const response = await fetch(`http://${window.location.hostname}:${port}/record`);
-// //     if (response.ok) {
-// //       const data = await response.json();
-// //       return data;
-// //     } else {
-// //       console.error("Failed to fetch records.");
-// //       return [];
-// //     }
-// //   } catch (error) {
-// //     console.error("An error occurred while fetching records:", error);
-// //     return [];
-// //   }
-// // }
+  // Function to fetch records from the backend
+  async function fetchRecords() {
+    try {
+      const response = await fetch(`http://${window.location.hostname}:${port}/record`);
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        console.error("Failed to fetch records.");
+        return [];
+      }
+    } catch (error) {
+      console.error("An error occurred while fetching records:", error);
+      return [];
+    }
+  }
 
-// // function OnloadRecordDisplay(RecordData){
-// //   RecordData.forEach(record => {
-// //     const markup = `<div class="col">
-// //                       <span>${record.first_name}</span>
-// //                       <span>${record.last_name}</span>
-// //                       <h4>${record.student_id}</h4>
-// //                       <h4>${record.email}</h4>
-// //                       <h4>${record.title}</h4>
-// //                       <h4>${record.type_of_work_id}</h4>
-// //                       <h4>${record.academic_year}</h4>
-// //                       <h4>${record.semester}</h4>
-// //                       <h4>${record.start_date}</h4>
-// //                       <h4>${record.end_date}</h4>
-// //                       <h4>${record.location}</h4>
-// //                       <h4>${record.description}</h4>
-// //                     </div>
-// //                     `;
+  function OnloadRecordDisplay(RecordData){
+    RecordData.forEach(record => {
+      const markup = `<div class="col">
+                        <span>${record.first_name}</span>
+                        <span>${record.last_name}</span>
+                        <h4>${record.student_id}</h4>
+                        <h4>${record.email}</h4>
+                        <h4>${record.title}</h4>
+                        <h4>${record.type_of_work_id}</h4>
+                        <h4>${record.academic_year}</h4>
+                        <h4>${record.semester}</h4>
+                        <h4>${record.start_date}</h4>
+                        <h4>${record.end_date}</h4>
+                        <h4>${record.location}</h4>
+                        <h4>${record.description}</h4>
+                      </div>
+                      `;
 
-// //     document.querySelector('.row').insertAdjacentHTML('beforeend', markup);
-// //   });
-// // }
+      document.querySelector('.row').insertAdjacentHTML('beforeend', markup);
+    });
+  }
 
-// //   // Event listener when the page content has finished loading
-// //   document.addEventListener("DOMContentLoaded", async () => {
-// //     const RecordData = await fetchRecords();
-// //     OnloadRecordDisplay(RecordData);
-// //   });
-
-//   // Create the data object to send to the backend
-//   const formData = new FormData(event.target);
-//   const data = {
-//     first_name: formData.get("fullname").split(" ")[0],
-//     last_name: formData.get("fullname").split(" ")[1],
-//     student_id: parseInt(formData.get("studentID")),
-//     email: formData.get("email"),
-//     title: formData.get("workTitle"),
-//     type_of_work_id: parseInt(formData.get("activityType")),
-//     academic_year: parseInt(formData.get("academicYear")) - 543,
-//     semester: parseInt(formData.get("semester")),
-//     start_date: formData.get("startDate"),
-//     end_date: formData.get("endDate"),
-//     location: formData.get("location"),
-//     description: formData.get("description")
-//   };
-
-//   console.log(data);
+    // Event listener when the page content has finished loading
+    document.addEventListener("DOMContentLoaded", async () => {
+      const RecordData = await fetchRecords();
+      OnloadRecordDisplay(RecordData);
+    });
 
 //   try {
 //     // Send data to the backend using POST request
@@ -435,14 +480,14 @@ function OnloadRecordDisplay(RecordData){
 //   }
 // }
 
-// // Event listener for form submission
-// document.getElementById("myForm").addEventListener("submit", submitForm);
+// Event listener for form submission
+document.getElementById("myForm").addEventListener("submit", submitForm);
 
-// // Event listeners for input validation on user input
-// document.getElementById("fullname").addEventListener("input", validateName);
-// document
-//   .getElementById("studentID")
-//   .addEventListener("input", validateStudentID);
-// document.getElementById("email").addEventListener("input", validateEmail);
+// Event listeners for input validation on user input
+document.getElementById("fullname").addEventListener("input", validateName);
+document
+  .getElementById("studentID")
+  .addEventListener("input", validateStudentID);
+document.getElementById("email").addEventListener("input", validateEmail);
 
 
