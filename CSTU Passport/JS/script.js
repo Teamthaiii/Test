135e945dc26b16774fbb1e77ceef1767cb0e0d72
TeamMie4K <Teamthai101@gmail.com>
2023-12-09 23:35:58 +0700

    test

--- CSTU Passport/JS/script.js
+++ CSTU Passport/JS/script.js
@@ -120,50 +120,77 @@ async function submitForm(event) {
   }
 
 
-// Function to fetch records from the backend
-async function fetchRecords() {
-  try {
-    const response = await fetch(`http://${window.location.hostname}:${port}/record`);
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      console.error("Failed to fetch records.");
-      return [];
-    }
-  } catch (error) {
-    console.error("An error occurred while fetching records:", error);
-    return [];
-  }
-}
+fetch(`http://${window.location.hostname}:${port}/record`)
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    data.forEach(record => {
+      const markup = `<div class="col">
+                        <span>${record.first_name}</span>
+                        <span>${record.last_name}</span>
+                        <h4>${record.student_id}</h4>
+                        <h4>${record.email}</h4>
+                        <h4>${record.title}</h4>
+                        <h4>${record.type_of_work_id}</h4>
+                        <h4>${record.academic_year}</h4>
+                        <h4>${record.semester}</h4>
+                        <h4>${record.start_date}</h4>
+                        <h4>${record.end_date}</h4>
+                        <h4>${record.location}</h4>
+                        <h4>${record.description}</h4>
+                      </div>
+                      `;
+      
+          document.querySelector('.row').insertAdjacentHTML('beforeend', markup);
+    });
+  })
+  .catch(error => console.log(error));
 
-function OnloadRecordDisplay(RecordData){
-  RecordData.forEach(record => {
-    const markup = `<div class="col">
-                      <span>${record.first_name}</span>
-                      <span>${record.last_name}</span>
-                      <h4>${record.student_id}</h4>
-                      <h4>${record.email}</h4>
-                      <h4>${record.title}</h4>
-                      <h4>${record.type_of_work_id}</h4>
-                      <h4>${record.academic_year}</h4>
-                      <h4>${record.semester}</h4>
-                      <h4>${record.start_date}</h4>
-                      <h4>${record.end_date}</h4>
-                      <h4>${record.location}</h4>
-                      <h4>${record.description}</h4>
-                    </div>
-                    `;
+// // Function to fetch records from the backend
+// async function fetchRecords() {
+//   try {
+//     const response = await fetch(`http://${window.location.hostname}:${port}/record`);
+//     if (response.ok) {
+//       const data = await response.json();
+//       return data;
+//     } else {
+//       console.error("Failed to fetch records.");
+//       return [];
+//     }
+//   } catch (error) {
+//     console.error("An error occurred while fetching records:", error);
+//     return [];
+//   }
+// }
 
-    document.querySelector('.row').insertAdjacentHTML('beforeend', markup);
-  });
-}
+// function OnloadRecordDisplay(RecordData){
+//   RecordData.forEach(record => {
+//     const markup = `<div class="col">
+//                       <span>${record.first_name}</span>
+//                       <span>${record.last_name}</span>
+//                       <h4>${record.student_id}</h4>
+//                       <h4>${record.email}</h4>
+//                       <h4>${record.title}</h4>
+//                       <h4>${record.type_of_work_id}</h4>
+//                       <h4>${record.academic_year}</h4>
+//                       <h4>${record.semester}</h4>
+//                       <h4>${record.start_date}</h4>
+//                       <h4>${record.end_date}</h4>
+//                       <h4>${record.location}</h4>
+//                       <h4>${record.description}</h4>
+//                     </div>
+//                     `;
 
-  // Event listener when the page content has finished loading
-  document.addEventListener("DOMContentLoaded", async () => {
-    const RecordData = await fetchRecords();
-    OnloadRecordDisplay(RecordData);
-  });
+//     document.querySelector('.row').insertAdjacentHTML('beforeend', markup);
+//   });
+// }
+
+//   // Event listener when the page content has finished loading
+//   document.addEventListener("DOMContentLoaded", async () => {
+//     const RecordData = await fetchRecords();
+//     OnloadRecordDisplay(RecordData);
+//   });
 
   // Create the data object to send to the backend
   const formData = new FormData(event.target);
